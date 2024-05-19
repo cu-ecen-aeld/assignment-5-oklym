cf0724bf6c81693ae7e5df90bd045e72de7b176b
OlegKlymenko <oklym@meta.ua>
2024-05-19 22:41:32 +0300

    start-stop script was added (to Assignment-3-and-latter repo)

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'c70cc68a2cc095e1d54505a53439842c424597b6'
+AESD_ASSIGNMENTS_VERSION = '6ed5e1ca1c149645020e968e58542537857af422'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
